f9ea2e4bdb4a6baffb8c93edcb62bdab48697608
kuyapp <alovei0927@gmail.com>
2014-07-04 11:17:33 +0800

    ordered channel and add style

--- get_links.py
+++ get_links.py
@@ -5,6 +5,7 @@ from lxml import etree
 from lxml import objectify
 import os
 import sys
+from collections import OrderedDict
 import requests
 from memoized import memoized
 
@@ -16,9 +17,9 @@ POST_DATA_BETA = '''<?xml version="1.0" encoding="UTF-8"?><request protocol="3.0
 
 POST_DATA_STABLE = '''<?xml version="1.0" encoding="UTF-8"?><request protocol="3.0" version="1.3.21.123" ismachine="0" sessionid="{12345678-1234-1234-1234-123456789012}" installsource="ondemandcheckforupdate" requestid="{12345678-1234-1234-1234-123456789012}"><os platform="win" version="6.1" sp="Service Pack 1" arch="x64" /><app appid="{4DC8B4CA-1BDA-483E-B5FA-D3C12E15B62D}" version="" nextversion="" ap="-multi-chrome" lang="" brand="GGLS" client=""><updatecheck /><ping active="1" /></app></request>'''
 
-post_data = {'dev':    POST_DATA_DEV,
-             'beta':   POST_DATA_BETA,
-             'stable': POST_DATA_STABLE}
+post_data = OrderedDict([('stable', POST_DATA_STABLE),
+              ('beta',   POST_DATA_BETA),
+              ('dev',    POST_DATA_DEV)])
 
 
 @memoized
@@ -26,7 +27,7 @@ def get_links(channel):
     print 'call with %s' % channel
     if channel not in post_data and channel != 'all':
         return None
-    result = {}
+    result = OrderedDict()
     for k, v in post_data.iteritems():
         if k == channel or channel == 'all':
             r = requests.post(API_URL, data=v)
---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+// select url text
+$(document).on('mouseenter', 'pre', function(e) {
+  var doc = document
+    , text = $(this).find('code')
+    , range, selection;
+
+  if(!text.length) return;
+
+  text = text[0];
+
+  // http://stackoverflow.com/a/987376/1189321
+  if (doc.body.createTextRange) {
+    range = doc.body.createTextRange();
+    range.moveToElementText(text);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();
+    range = doc.createRange();
+    range.selectNodeContents(text);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+});
